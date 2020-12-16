f1f48e56c584ec981f89e5cc65a5e4bf013a32eb
iqbalandras <75374076+iqbalandras@users.noreply.github.com>
2020-12-16 16:57:47 +0700

    Add files via upload

---
+++ data/LANDMARK_5.js
@@ -0,0 +1,11 @@
+var json_LANDMARK_5 = {
+"type": "FeatureCollection",
+"name": "LANDMARK_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "301", "id": "here:pds:place:360qqvbz-d078202e8264439f97868f3ccbca76d3", "origin_id": 4.0, "title": "Km 0 Pekalongan", "label": "Km 0 Pekalongan, Jalan Diponegoro, Pekalongan Kota 40384, Indonesia", "distance": 1974.0, "categories": "300-3000-0000;300-3000-0025" }, "geometry": { "type": "Point", "coordinates": [ 109.67423, -6.87908 ] } },
+{ "type": "Feature", "properties": { "fid": "302", "id": "here:pds:place:360qqvbz-c79b1fb33f4a4e5c843dff1098c6664f", "origin_id": 4.0, "title": "Tugu Adipura Kota Pekalongan", "label": "Tugu Adipura Kota Pekalongan, Jalan Dr. Cipto, Pekalongan Kota 51122, Indonesia", "distance": 2338.0, "categories": "300-3000-0025" }, "geometry": { "type": "Point", "coordinates": [ 109.68042, -6.89025 ] } },
+{ "type": "Feature", "properties": { "fid": "810", "id": "here:pds:place:360qqy0b-bb1d3b2793b348118108d33051e9e618", "origin_id": 9.0, "title": "Monumen Selamat Datang", "label": "Monumen Selamat Datang, Ki. Mangun Sarkoro, Pekalongan Kota 51123, Indonesia", "distance": 3406.0, "categories": "300-3000-0025" }, "geometry": { "type": "Point", "coordinates": [ 109.70666, -6.89198 ] } },
+{ "type": "Feature", "properties": { "fid": "1009", "id": "here:pds:place:360qqvbz-843331fb7945441b91a9ccca06d18b70", "origin_id": 11.0, "title": "Monumen Juang 3 Oktober", "label": "Monumen Juang 3 Oktober, Jalan Presiden 17, Pekalongan Kota 51119, Indonesia", "distance": 3718.0, "categories": "300-3000-0025" }, "geometry": { "type": "Point", "coordinates": [ 109.66815, -6.88771 ] } }
+]
+}
